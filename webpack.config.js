87323806a2c33c06040ba1077acd4a5fcf75b15d
Caio Ferrarezi <caioferrarezi2@gmail.com>
2021-01-02 15:41:59 -0300

    improve development mode

--- webpack.config.js
+++ webpack.config.js
@@ -3,32 +3,36 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: '[name]-[hash].js',
-    path: path.resolve(__dirname, 'demo')
-  },
-  resolve: {
-    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-    extensions: ['.js']
-  },
-  module: {
-    rules: [{
-      test: /\.m?js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
+module.exports = env => {
+  return {
+    mode: env.NODE_ENV,
+    devtool: 'inline-source-map',
+    entry: './src/index.js',
+    output: {
+      filename: '[name]-[hash].js',
+      path: path.resolve(__dirname, 'demo')
+    },
+    resolve: {
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+      extensions: ['.js']
+    },
+    module: {
+      rules: [{
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
         }
-      }
-    }],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ]
+      }],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html'
+      })
+    ]
+  }
 }
